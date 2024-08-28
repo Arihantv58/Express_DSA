950f0d99d193b8e50933232ef837e7581cb0105c
Arihant Vidhyarthi <arihantvidhyarthibtech@delhitechnicalcampus.ac.in>
2024-08-28 12:56:14 +0530

    second

--- first.java
+++ first.java
@@ -3,9 +3,12 @@ public class first {
     public static void main(String[] args) {
         int a,b;
         Scanner sc = new Scanner(System.in);
+        System.out.println("Ener first number that you want to add : ");
         a=sc.nextInt();
+        System.out.println("Enter second number that you want to add : ");
         b=sc.nextInt();
         int c= a+b;
+        System.out.println("Hello Thundergod! here is your required result : ");
         System.out.println(c);
     }
 }
