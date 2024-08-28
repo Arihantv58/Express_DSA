2c038eb879265c897355a6c0e2bc61ea698d86f8
Arihant Vidhyarthi <arihantvidhyarthibtech@delhitechnicalcampus.ac.in>
2024-08-28 12:38:24 +0530

    Addition program

---
+++ first.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class first {
+    public static void main(String[] args) {
+        int a,b;
+        Scanner sc = new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        int c= a+b;
+        System.out.println(c);
+    }
+}
