a645ed4d6df2e44c42fbfb64038947ef23a29ff3
Arihant Vidhyarthi <arihantvidhyarthibtech@delhitechnicalcampus.ac.in>
2024-12-25 12:45:57 +0530

    Flloyd Triangle Pattern

---
+++ Patterns/Pattern13.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+public class Pattern13 {
+    public static void main(String[] args) {
+        int num = 1;
+        int n = 5;
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print(num+ " ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
