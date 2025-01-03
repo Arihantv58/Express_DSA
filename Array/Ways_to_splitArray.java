2fbd486cd9df75d83c7c4c108bf44e25e7af3bc5
Arihant Vidhyarthi <arihantvidhyarthibtech@delhitechnicalcampus.ac.in>
2025-01-03 20:15:39 +0530

    Array Problem- Leetcode 2270

---
+++ Array/Ways_to_splitArray.java
@@ -0,0 +1,34 @@
+package Array;
+import java.util.*;
+
+public class Ways_to_splitArray {
+    public static int waysToSplitArray(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        long total = 0;
+        for(int i=0; i<n; i++)
+        {
+            total+=nums[i];
+        }
+        
+        long currSum = 0;
+        for(int i=0; i<n-1; i++)
+        {
+            currSum += nums[i];
+            if(currSum >= total-currSum)
+                count++;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for(int i=0; i<n; i++)
+        {
+            nums[i] = sc.nextInt();
+        }
+        System.out.println(waysToSplitArray(nums));
+    }
+}
