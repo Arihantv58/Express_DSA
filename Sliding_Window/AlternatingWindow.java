f66d1b8d599508dd3d750b228f7c5b475ce17481
Arihant Vidhyarthi <arihantvidhyarthibtech@delhitechnicalcampus.ac.in>
2025-03-09 22:02:56 +0530

    Sliding Window

---
+++ Sliding_Window/AlternatingWindow.java
@@ -0,0 +1,30 @@
+package Sliding_Window;
+
+public class AlternatingWindow {
+    public static int numberOfAlternatingGroups(int[] colors, int k) {
+        int n = colors.length;
+        int[] temp = new int[n + k - 1];
+        System.arraycopy(colors, 0, temp, 0, n);
+        System.arraycopy(colors, 0, temp, n, k - 1); 
+    
+        int count = 0;
+        int left = 0;
+        
+        for (int right = 0; right < temp.length; right++) {
+            if (right > 0 && temp[right] == temp[right - 1]) {
+                left = right;  
+            }
+            
+            if (right - left + 1 >= k) {
+                count++;  
+            }
+        }
+        
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] colors = {0,1,0,0,1,0,1};
+        int k = 6;
+        System.out.println(numberOfAlternatingGroups(colors, k));
+    }
+}
